43c3baf42fb282c2dcceccdb639fa1d3fb2af9cd
GuoJikun <guojikun@y-open.com>
2021-09-16 11:01:37 +0800

    init

---
+++ src/.vuepress/config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  base: "/",
+  themeConfig: {
+    home: "p2.md",
+    docsDir: "src",
+    prev: true,
+    sidebar: [
+      {
+        text: "p1",
+        link: "/p1.md",
+      },
+      {
+        text: "p2",
+        link: "/p2.md",
+      },
+      {
+        text: "p3",
+        link: "/p3.md",
+      },
+    ],
+  },
+};
